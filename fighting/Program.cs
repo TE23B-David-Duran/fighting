e04db3fdf6f53cbfaeca00455849b8c9360fb5cc
David Duran <david.duranherrera@elev.ga.ntig.se>
2024-09-25 15:00:51 +0200

    Snart klar med pvp koden

--- fighting/Program.cs
+++ fighting/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int hp1 = Random.Shared.Next(80, 100);
+int hp2 = Random.Shared.Next(80, 100);
+
+string name1;
+string name2;
+
+Console.WriteLine("Skriv namn på player 1 och player 2");
+name1 = Console.ReadLine();
+name2 = Console.ReadLine();
+
+Console.WriteLine($"{name1} vs {name2} ");
+Console.ReadLine();
+
+
+while(hp1 > 0 || hp2 > 0)
+{
+Console.WriteLine($"{name1}: {hp1}");
+int damage1 = Random.Shared.Next(4, 10);
+hp1 -= damage1;
+Console.WriteLine($"{name2} did {damage1} damage to {name1}");
+  Thread.Sleep(1000);  
+Console.WriteLine($"{name1}: {hp1}");
+Console.WriteLine($"{name2}: {hp2}");
+int damage2 = Random.Shared.Next(4, 10);
+hp2 -= damage2;
+Console.WriteLine($"{name1} did {damage2} damage to {name2}");
+Thread.Sleep(1000);  
+Console.WriteLine($"{name2}: {hp2}"); 
+}
+Console.ReadLine();
+
+
+
